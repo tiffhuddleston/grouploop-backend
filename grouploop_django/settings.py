b8626aa2d7ca9bc100276a76dd5d9d7376edb9b9
tiffhuddleston <tiffhuddleston@gmail.com>
2020-03-06 15:03:16 -0600

    update settings

--- grouploop_django/settings.py
+++ grouploop_django/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
+import django_heroku
 import dj_database_url
 import os
 from decouple import config, Csv
@@ -141,3 +142,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
