c172ec5524b276763577cd13db09f227b526a16a
tiffhuddleston <tiffhuddleston@gmail.com>
2020-03-06 14:23:45 -0600

    update settings

--- grouploop_django/settings.py
+++ grouploop_django/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+ALLOWED_HOSTS = ['.localhost', '.herokuapp.com']
 
 
 # Application definition
