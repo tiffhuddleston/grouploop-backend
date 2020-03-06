bbc0284db372348d3b6eaa90d2e5e77fe68825e7
tiffhuddleston <tiffhuddleston@gmail.com>
2020-03-06 16:11:28 -0600

    Attempting deploy changes

--- grouploop_django/settings.py
+++ grouploop_django/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.localhost', '.herokuapp.com']
+ALLOWED_HOSTS = ['.localhost', 'grouploop-be.herokuapp.com']
 
 
 # Application definition
