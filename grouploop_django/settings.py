66c77e1b0f179c3b50e672027e5a73a56ff4b62a
tiffhuddleston <tiffhuddleston@gmail.com>
2020-03-11 13:44:29 -0500

    unrequired model fields

--- grouploop_django/settings.py
+++ grouploop_django/settings.py
@@ -48,12 +48,18 @@ INSTALLED_APPS = [
 
 ]
 
+# REST_FRAMEWORK = {
+#     # Use Django's standard `django.contrib.auth` permissions,
+#     # or allow read-only access for unauthenticated users.
+#     'DEFAULT_PERMISSION_CLASSES': [
+#         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+#     ]
+# }
+
 REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    # other settings...
+    'DEFAULT_AUTHENTICATION_CLASSES': [],
+    'DEFAULT_PERMISSION_CLASSES': [],
 }
 
 CORS_ORIGIN_WHITELIST = [
